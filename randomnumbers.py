b3b93e8e81edd7ff061a5c0f27659e707944fcdb
CarelessOutcast <tapiacarlos32@gmail.com>
2023-03-06 15:42:00 -0600

    Homework 9 done

--- randomnumbers.py
+++ randomnumbers.py
@@ -2,13 +2,14 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
 
 
 #define linear congruential generator
 
 def LCG(m,a,b,x0,N):
     seq=[]
-    for i in range(N):
+    for _ in range(N):
         x = (a * x0 + b) % m
         seq.append(float(x))
         x0 = x
@@ -25,43 +26,44 @@ def g(x):
 
 
 #plot of the functions
-x = np.linspace(-0.5,1.5,100)
-y1 = f(x)
-y2 = g(x)
-plt.plot(x,y1)
-plt.plot(x,y2)
-plt.show()
+# x = np.linspace(-0.5,1.5,100)
+# y1 = f(x)
+# y2 = g(x)
+# plt.plot(x,y1)
+# plt.plot(x,y2)
+# plt.show()
 
 
 # We use  Monte Carlo Type I simulation: 
 # area equals the average of "g-f" over [0,1] x the size of the interval 
 
-xvalues = np.array(LCG(2**31-1,7**5,0,3,10000))/(2**31-1)
-
-yvalues = g(xvalues)-f(xvalues)
-
-mc1 = sum(yvalues) / len(yvalues) 
-
-print('\nMonte Carto (type I) estimate:',mc1)
-
-# We use  Monte Carlo Type II simulation: 
-# count how many land inside the region of interest 
-
-count = 0
-for i in range(len(xvalues)-1):
-    if xvalues[i+1] >= f(xvalues[i]) and xvalues[i+1] <= g(xvalues[i]):
-        count = count + 1
-
-area = 1
-mc2 = count/(len(xvalues)-1) * area
-
-print('\nMonte Carto (type II) estimate:',mc2)
-
-
-
+# xvalues = np.array(LCG(2**31-1,7**5,0,3,104))/(2**31-1)
+#
+# yvalues = g(xvalues)-f(xvalues)
+#
+# mc1 = sum(yvalues) / len(yvalues) 
+#
+# print('\nMonte Carto (type I) estimate:',mc1)
+#
+# # We use  Monte Carlo Type II simulation: 
+# # count how many land inside the region of interest 
+#
+# count = 0
+# for i in range(len(xvalues)-1):
+#     if xvalues[i+1] >= f(xvalues[i]) and xvalues[i+1] <= g(xvalues[i]):
+#         count = count + 1
+#
+# area = 1
+# mc2 = count/(len(xvalues)-1) * area
+#
+# print('\nMonte Carto (type II) estimate:',mc2)
+#
+#
+#
 #Volume of the unit sphere
 
-print('\nVolume of unit sphere:\n')
+print('\nVolume of unit sphere:')
+print(f'Expected: \t{(np.pi/24)}')
 
 # We use now a Monte Carlo Type II simulation to find the volume of the unit sphere: 
 #                        
@@ -69,16 +71,72 @@ print('\nVolume of unit sphere:\n')
 #
 #We enclose the unit sphere in the cube [-1,1]x[-1,1]x[-1,1], sample random points from this unit cube and count how many land inside the spherxval
 
-xvaluesn = 2*xvalues - 1  #we generate points in [-1,1] from the original set in [0,1]
+# xvaluesn = 2*xvalues - 1  #we generate points in [-1,1] from the original set in [0,1]
+# count3d = 0
+# for i in range(len(xvaluesn)-2):
+#     x=xvaluesn[i]; y=xvaluesn[i+1]; z=xvaluesn[i+2];
+#     if x**2 + y**2 + z**2 <= 1:
+#         count3d = count3d + 1
+#
+# vol = 8
+# mc2 = count3d/(len(xvaluesn)-2) * vol
+#
+# print('Monte Carto (type II) estimate:',mc2)
+# print('\nVolume of unit sphere:')
+#
 count3d = 0
-for i in range(len(xvaluesn)-2):
-    x=xvaluesn[i]; y=xvaluesn[i+1]; z=xvaluesn[i+2];
-    if x**2 + y**2 + z**2 <= 1:
+xpoints = []
+ypoints = []
+zpoints = []
+
+xpoints1 = []
+ypoints1 = []
+zpoints1 = []
+
+N = 10**4
+uvaluesn = np.array(LCG(2**31-1,7**5,0,1,N))/(2**31-1)
+for i in range(len(uvaluesn)-2):
+    x=uvaluesn[i];
+    y=uvaluesn[i+1];
+    z=uvaluesn[i+2];
+    if 2 + 4*x**2 + y**2 + 4*z**2 <= 4*x+4*z+y:
         count3d = count3d + 1
+        xpoints.append(x)
+        ypoints.append(y)
+        zpoints.append(z)
 
-vol = 8
-mc2 = count3d/(len(xvaluesn)-2) * vol
+vol_box = 1  # the volume of the enclosing box is 2x2x2=8
+mc2 = count3d/(len(uvaluesn)-2) * vol_box
 
-print('Monte Carto (type II) estimate:',mc2)
+N = 10**6
+uvaluesn = np.array(LCG(2**31-1,7**5,0,1,N))/(2**31-1)
+for i in range(len(uvaluesn)-2):
+    x=uvaluesn[i];
+    y=uvaluesn[i+1];
+    z=uvaluesn[i+2];
+    if 2 + 4*x**2 + y**2 + 4*z**2 <= 4*x+4*z+y:
+        count3d = count3d + 1
+        xpoints1.append(x)
+        ypoints1.append(y)
+        zpoints1.append(z)
+vol_box = 1  # the volume of the enclosing box is 2x2x2=8
+mc2_1 = count3d/(len(uvaluesn)-2) * vol_box
 
+N = 10**4
+print(f'\nN = {N:,}')
+print(f'MC(type II):\t{mc2}')
+print(f'Error: \t{mc2 - (np.pi/24)}')
+
+N = 10**6
+print(f'\nN = {N:,}')
+print(f'MC(type II):\t{mc2_1}')
+print(f'Error :\t{mc2_1- (np.pi/24)}')
+# print(f'pi/24 = \t{np.pi/24}')
+
+fig, ax = plt.subplots(subplot_kw={"projection":"3d"})
+fig, ax2 = plt.subplots(subplot_kw={"projection":"3d"})
+
+ax.scatter(xpoints1,ypoints1,zpoints1,s=1, color="red")
+ax2.scatter(xpoints,ypoints,zpoints,s=1 )
+# plt.show()
 
--- randomwalk.py
+++ randomwalk.py
@@ -3,8 +3,8 @@
 
 import random
 
-
-def randwalk():
+def randwalk(b,a):
+    avar = 0
     count = 0
     pos = 0
     while pos < a and b < pos:
@@ -13,24 +13,51 @@ def randwalk():
         else:
             pos = pos - 1
         count = count + 1
-    return (count)
+    if pos==a:
+        avar = 1
+    return [count,avar]
     
 
 
 # Estimate the escape time for one simulation
- 
-a = 6
-b = -3
+#  #(a)
+b = -2
+a = 5
 
-print('Exit time (one trial):', randwalk())
+#  #(b)
+# a = 3
+# b = -5
+#
+ #(c)
+# b = -8
+# a = 3
 
+print(f'Exit time (one trial): {randwalk(b,a)[0]}')
+values = []
 # Estimate the escape time for N simulations
 
 N = 10000
+probs = 0
 steps = 0
 for n in range(N):
-    steps = steps + randwalk()
-print('\nExpected Exit time after',N,'trials:',steps/N)
-    
-                               
+    values.append(0)
+    values[n], avar = randwalk(b,a)
+    probs += avar;
+    steps += values[n]
+
+variance = 0
+for n in range(N):
+    variance += (values[n]-(steps/N))**2
+
+print(f"\n  For {N} trials")
+print('Sample Mean:&\t',steps/N)
+print('Variance:&\t',variance/(N-1))
+print('Standard Err:&\t',(variance/(N-1))**(1/2))
+print(f'Actual prob:&\t {b/(b-a)}')
+print(f'Cal. prob:&\t {probs/N}')
+print(f'prob\\,\\,Error:&\t {b/(b-a)-probs/N}')
+
+
+
+
 
